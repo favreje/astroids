7b515b4af9fca09c9116bdc6a15e1e46718a69ef
favreje <favreje@gmail.com>
2024-09-07 21:36:11 -0500

    Added asteroid objects

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from asteroid import Asteroid
+from asteroidfield import *
 import pygame
 from constants import *
 from player import Player
@@ -6,15 +8,25 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
-        player.draw(screen)
-        player.update(dt)
+        for action in drawable:
+            action.draw(screen)
+        for action in updatable:
+            action.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -17,7 +17,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        return pygame.draw.polygon(screen, WHITE, self.triangle(), width= 2)
+        pygame.draw.polygon(screen, WHITE, self.triangle(), width= 2)
 
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
